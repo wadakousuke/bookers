74985e5de451c8bbb61f58fac8a403431d35ff1f
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-03-13 13:50:12 +0000

    [Add 削除機能　フラッシュメッセージの追加]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+     @book = Book.find(params[:id])
   end
 
   def show
@@ -17,7 +17,32 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
+    if book.save
+      flash[:notice] = "Succesfully 新規追加に成功しました"
+    else
+      flash.now[:notice] = "新規追加に失敗しました"
+    end
+    redirect_to "/books/#{book.id}"
+
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Succesfully 更新に成功しました"
+    else
+      flash.now[:noticer] = "更新に失敗しました"
+    end
+    redirect_to "/books/#{book.id}"
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+       flash[:notice] = "Succesfully 削除に成功しました"
+    else
+      flash.now[:notice] = "更新に失敗しました"
+    end
     redirect_to "/books"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   post "books" => "books#create"
   get "books" => "books#index"
-  get "books/id" => "books#show"
-  get 'top' => "homes#top"
-  get "books/:id/edit" => "books#edit", as: "edit_book"
   get "books/:id" => "books#show"
+  get 'top' => "homes#top"
+  get "books/:id/edit" => "books#edit"
+  patch "books/:id" => "books#update" , as: "update_book"
+  delete "books/:id" => "books#destroy" , as: "destroy_book"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
