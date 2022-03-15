49724662e070b1aa8efc1ed929e20dc6193909d7
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-03-15 14:52:47 +0000

    [Modify テスト結果の修正]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Succesfully 新規追加に成功しました"
+      flash[:notice] = "successfully 新規追加に成功しました"
       redirect_to "/books/#{@book.id}"
     else
       @books = Book.all
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Succesfully 更新に成功しました"
+      flash[:notice] = "successfully 更新に成功しました"
       redirect_to "/books/#{@book.id}"
     else
 
@@ -43,7 +43,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     if book.destroy
-       flash[:notice] = "Succesfully 削除に成功しました"
+       flash[:notice] = "successfully 削除に成功しました"
     else
       flash.now[:notice] = "更新に失敗しました"
     end
